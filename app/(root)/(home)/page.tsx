1bf05738211ce89aed26b5778a5c3a72b29f8393
SzTamass <sz.tamas@camelcoding.com>
2024-06-29 20:58:13 +0300

    initial commit + theme configuration

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { BackgroundBeams } from "@/components/BeamsBackground";
+import { ModeToggle } from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+    <main>
+      <BackgroundBeams />
+      <ModeToggle />
+    </main>
+  );
+}
