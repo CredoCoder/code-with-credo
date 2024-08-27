71d2c8774da0978c09eb0576f918162d09dda7bf
CredoCoder <s.tamaska39@gmail.com>
2024-08-27 14:34:41 +0300

    -fixed vercel analytics issue

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,39 +10,6 @@ const getProjects = async () => {
   return projects;
 };
 
-export const metadata: Metadata = {
-  title: "Code With Credo",
-  description: "Hi there! I'm Szakacs Tamas a Frontend Developer! Get to know me!",
-  keywords: [
-    "Frontend",
-    "Developer",
-    "Frontend Developer",
-    "Szakacs Tamas",
-    "Szakács Tamás",
-    "web application",
-    "website",
-    "next.js developer",
-    "react developer",
-    "code with credo",
-  ],
-  icons: {
-    icon: "https://code-with-credo.vercel.app/images/logo.png",
-  },
-  openGraph: {
-    title: "Code With Credo",
-    description: "Hi there! I'm Szakacs Tamas a Frontend Developer! Get to know me!",
-    url: "https://code-with-credo.vercel.app/",
-    siteName: "Code With Credo",
-    images: [
-      {
-        url: "https://code-with-credo.vercel.app/images/hero.jpg",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
-
 export default async function Home() {
   return (
     <>
